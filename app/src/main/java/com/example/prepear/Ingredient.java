8d1dd4b92922bf0054aa686c136294baa2e7fb8c
yingyue3 <yingyue3@ualberta.ca>
2022-10-23 23:22:09 -0600

    add recipe and ingredient class

---
+++ app/src/main/java/com/example/prepear/Ingredient.java
@@ -0,0 +1,47 @@
+package com.example.prepear;
+
+public class Ingredient {
+    String briefDescription;
+    Integer amount;
+    Integer unit;
+    String ingredientCategory;
+
+    public Ingredient(String briefDescription, int amount, int unit, String ingredientCategory) {
+        this.briefDescription = briefDescription;
+        this.amount = amount;
+        this.unit = unit;
+        this.ingredientCategory = ingredientCategory;
+    }
+
+    public String getBriefDescription() {
+        return briefDescription;
+    }
+
+    public void setBriefDescription(String briefDescription) {
+        this.briefDescription = briefDescription;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public Integer getUnit() {
+        return unit;
+    }
+
+    public void setUnit(Integer unit) {
+        this.unit = unit;
+    }
+
+    public String getIngredientCategory() {
+        return ingredientCategory;
+    }
+
+    public void setIngredientCategory(String ingredientCategory) {
+        this.ingredientCategory = ingredientCategory;
+    }
+}
---
+++ app/src/main/java/com/example/prepear/Recipe.java
@@ -0,0 +1,70 @@
+package com.example.prepear;
+
+import java.util.ArrayList;
+
+public class Recipe {
+    String title;
+    Integer preparationTime;
+    Integer numberOfServings;
+    String recipeCategory;
+    String comments;
+    ArrayList<Ingredient> listOfIngredients;
+
+    public Recipe(String title, Integer preparationTime,Integer numberOfServings, String recipeCategory, String comments) {
+        this.title = title;
+        this.preparationTime = preparationTime;
+        this.numberOfServings = numberOfServings;
+        this.recipeCategory = recipeCategory;
+        this.comments = comments;
+    }
+
+
+    public Ingredient getIngredientsByIndex(Integer indexOfIngredient) {
+        return this.listOfIngredients.get(indexOfIngredient);
+    }
+
+    public void setIngredient(Ingredient ingredient){
+        this.listOfIngredients.add(ingredient);
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPreparationTime() {
+        return preparationTime;
+    }
+
+    public void setPreparationTime(Integer preparationTime) {
+        this.preparationTime = preparationTime;
+    }
+
+    public Integer getNumberOfServings() {
+        return numberOfServings;
+    }
+
+    public void setNumberOfServings(Integer numberOfServings) {
+        this.numberOfServings = numberOfServings;
+    }
+
+    public String getRecipeCategory() {
+        return recipeCategory;
+    }
+
+    public void setRecipeCategory(String recipeCategory) {
+        this.recipeCategory = recipeCategory;
+    }
+
+    public String getComments() {
+        return comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+}
+
