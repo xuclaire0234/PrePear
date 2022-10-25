a53c19396def946f33a980bd6bf7c0b5cbeb9d7f
Jiayin He <jiayin7@ualberta.ca>
2022-10-25 14:21:10 -0600

    1st draft of recipe part project without connecting ingredients in recipe to the database

---
+++ app/src/main/java/com/example/prepear/CustomIngredientInRecipeList.java
@@ -0,0 +1,50 @@
+package com.example.prepear;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.ArrayList;
+
+public class CustomIngredientInRecipeList extends ArrayAdapter<IngredientInRecipe> {
+
+    private ArrayList<IngredientInRecipe> ingredientsInRecipe;
+    private Context context;
+
+    public CustomIngredientInRecipeList(Context context, ArrayList<IngredientInRecipe> ingredientsInRecipe) {
+        super(context, 0, ingredientsInRecipe);
+        this.ingredientsInRecipe = ingredientsInRecipe;
+        this.context = context;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        // return super.getView(position, convertView, parent);
+        View view = convertView;
+
+        if (view == null) {
+            view = LayoutInflater.from(context).inflate(R.layout.content_ingredient_in_recipe, parent, false);
+        }
+
+        IngredientInRecipe ingredientInRecipe = ingredientsInRecipe.get(position);
+
+        TextView briefDescriptionTextView = view.findViewById(R.id.brief_description_TextView);
+        TextView amountTextView = view.findViewById(R.id.amount_TextView);
+        TextView unitTextView = view.findViewById(R.id.unit_TextView);
+        TextView ingredientCategoryTextView = view.findViewById(R.id.ingredient_category_TextView);
+
+        briefDescriptionTextView.setText(ingredientInRecipe.getBriefDescription());
+        amountTextView.setText(ingredientInRecipe.getAmount());
+        unitTextView.setText(ingredientInRecipe.getUnit());
+        ingredientCategoryTextView.setText(ingredientInRecipe.getIngredientCategory());
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/prepear/Recipe.java
@@ -0,0 +1,90 @@
+package com.example.prepear;
+
+import android.net.Uri;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+
+public class Recipe implements Serializable {
+    Uri imageURI;
+    String title;
+    Integer preparationTime;
+    Integer numberOfServings;
+    String recipeCategory;
+    String comments;
+    ArrayList<IngredientInRecipe> listOfIngredients;
+
+    public Recipe(Uri imageURI, String title, Integer preparationTime,Integer numberOfServings, String recipeCategory, String comments, ArrayList<IngredientInRecipe> listOfIngredients) {
+        this.imageURI = imageURI;
+        this.title = title;
+        this.preparationTime = preparationTime;
+        this.numberOfServings = numberOfServings;
+        this.recipeCategory = recipeCategory;
+        this.comments = comments;
+        this.listOfIngredients = listOfIngredients;
+    }
+
+    public Uri getImageURI() {
+        return this.imageURI;
+    }
+
+    public void setImageURI(Uri imageURI) {
+        this.imageURI = imageURI;
+    }
+
+    public String getTitle() {
+        return this.title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public Integer getPreparationTime() {
+        return this.preparationTime;
+    }
+
+    public void setPreparationTime(Integer preparationTime) {
+        this.preparationTime = preparationTime;
+    }
+
+    public Integer getNumberOfServings() {
+        return this.numberOfServings;
+    }
+
+    public void setNumberOfServings(Integer numberOfServings) {
+        this.numberOfServings = numberOfServings;
+    }
+
+    public String getRecipeCategory() {
+        return this.recipeCategory;
+    }
+
+    public void setRecipeCategory(String recipeCategory) {
+        this.recipeCategory = recipeCategory;
+    }
+
+    public String getComments() {
+        return this.comments;
+    }
+
+    public void setComments(String comments) {
+        this.comments = comments;
+    }
+
+    public ArrayList<IngredientInRecipe> getListOfIngredients() {
+        return this.listOfIngredients;
+    }
+
+    public void setListOfIngredients(ArrayList<IngredientInRecipe> listOfIngredients) {
+        this.listOfIngredients = listOfIngredients;
+    }
+
+    public IngredientInRecipe getIngredientByIndex(Integer indexOfIngredient) {
+        return this.listOfIngredients.get(indexOfIngredient);
+    }
+
+    public void addIngredientToRecipe(IngredientInRecipe ingredient){
+        this.listOfIngredients.add(ingredient);
+    }
+}
