ec31124b931a06f6eb6eaa17b07689e7e35b62da
Shihao Liu <lsh20011018@gmail.com>
2022-10-25 03:50:13 -0600

    Initialize Classes only

---
+++ app/src/main/java/com/example/prepear/IngredientStorageController.java
@@ -0,0 +1,4 @@
+package com.example.prepear;
+
+public class IngredientStorageController {
+}
---
+++ app/src/main/java/com/example/prepear/ViewIngredientStorage.java
@@ -0,0 +1,13 @@
+package com.example.prepear;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewIngredientStorage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_ingredient_storage);
+    }
+}
