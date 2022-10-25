8682119072c82e65ca529edafcad6b769c36fedd
Shihao Liu <lsh20011018@gmail.com>
2022-10-24 23:38:29 -0600

    Keep Newest Version of The Project on My End

--- app/src/main/java/com/example/prepear/MainActivity.java
+++ app/src/main/java/com/example/prepear/MainActivity.java
@@ -6,20 +6,11 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-
-<<<<<<< HEAD
-
-
-=======
->>>>>>> a88cfa23524896b3f593a32741a2d96cffde3fd6
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-<<<<<<< HEAD
-=======
 
->>>>>>> a88cfa23524896b3f593a32741a2d96cffde3fd6
     }
 
 }
