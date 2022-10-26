9d4dcf2f6c98eee055f093611a8ec9565f01fb50
Jamie Lee <jamie.lee.jungeun@gmail.com>
2022-10-26 16:20:04 -0600

    edited fragment java files

---
+++ app/src/main/java/com/example/prepear/IngredientInRecipe.java
@@ -0,0 +1,49 @@
+package com.example.prepear;
+
+import io.grpc.internal.DnsNameResolver;
+
+public class IngredientInRecipe {
+    String briefDescription;
+    Integer amount;
+    String unit;
+    String ingredientCategory;
+
+    public IngredientInRecipe(String briefDescription, int amount, String unit, String ingredientCategory) {
+        this.briefDescription = briefDescription;
+        this.amount = amount;
+        this.unit = unit;
+        this.ingredientCategory = ingredientCategory;
+    }
+
+    public String getBriefDescription() {
+        return briefDescription;
+    }
+
+    public void setBriefDescription(String briefDescription) {
+        this.briefDescription = briefDescription;
+    }
+
+    public Integer getAmount() {
+        return amount;
+    }
+
+    public void setAmount(Integer amount) {
+        this.amount = amount;
+    }
+
+    public String getUnit() {
+        return unit;
+    }
+
+    public void setUnit(String unit) {
+        this.unit = unit;
+    }
+
+    public String getIngredientCategory() {
+        return ingredientCategory;
+    }
+
+    public void setIngredientCategory(String ingredientCategory) {
+        this.ingredientCategory = ingredientCategory;
+    }
+}
---
+++ app/src/main/java/com/example/prepear/ViewRecipeActivity.java
@@ -0,0 +1,90 @@
+package com.example.prepear;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+
+public class ViewRecipeActivity extends AppCompatActivity {
+    private ImageView imageImageView;
+    private TextView titleTextView;
+    private TextView preparationTimeTextView;
+    private TextView numberOfServingsTextView;
+    private TextView recipeCategoryTextView;
+    private TextView commentsTextView;
+    private ListView ingredientInRecipeListView;
+    private Button editButton;
+    private Button deleteButton;
+    private Button returnButton;
+    private ArrayAdapter<IngredientInRecipe> ingredientInRecipeArrayAdapter;
+    private ArrayList<IngredientInRecipe> ingredientInRecipeDataList;
+    private Recipe viewedRecipe;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_recipe);
+
+        imageImageView = findViewById(R.id.imageView);
+        titleTextView = findViewById(R.id.title_TextView);
+        preparationTimeTextView = findViewById(R.id.preparation_time_TextView);
+        numberOfServingsTextView = findViewById(R.id.number_of_servings_TextView);
+        recipeCategoryTextView = findViewById(R.id.recipe_category_TextView);
+        commentsTextView = findViewById(R.id.comments_TextView);
+        ingredientInRecipeListView = findViewById(R.id.ingredient_in_recipe_ListView);
+        editButton = findViewById(R.id.confirm_button);
+        deleteButton = findViewById(R.id.delete_button);
+        returnButton = findViewById(R.id.cancel_button);
+
+        viewedRecipe = (Recipe) getIntent().getSerializableExtra("viewed recipe");
+        imageImageView.setImageURI(viewedRecipe.getImageURI());
+        titleTextView.setText(viewedRecipe.getTitle());
+        preparationTimeTextView.setText(viewedRecipe.getPreparationTime().toString());
+        numberOfServingsTextView.setText(viewedRecipe.getNumberOfServings().toString());
+        recipeCategoryTextView.setText(viewedRecipe.getRecipeCategory());
+        commentsTextView.setText(viewedRecipe.getComments());
+
+        ingredientInRecipeDataList = viewedRecipe.getListOfIngredients();
+        ingredientInRecipeArrayAdapter = new CustomIngredientInRecipeList(this, ingredientInRecipeDataList);
+        ingredientInRecipeListView.setAdapter(ingredientInRecipeArrayAdapter);
+
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent switchActivityIntent = new Intent(ViewRecipeActivity.this, AddEditRecipeActivity.class);
+                switchActivityIntent.putExtra("viewed recipe", viewedRecipe);
+                startActivity(switchActivityIntent);
+            }
+        });
+
+        deleteButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent returnIntent = new Intent();
+                returnIntent.putExtra("delete recipe", viewedRecipe);
+                setResult(Activity.RESULT_OK, returnIntent);
+                finish();
+            }
+        });
+
+        returnButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent returnIntent = new Intent();
+                setResult(Activity.RESULT_CANCELED, returnIntent);
+                finish();
+            }
+        });
+    }
+}
