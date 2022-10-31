d212056461864679b1249f1bf292f1e72222dfa0
Jiayin He <jiayin7@ualberta.ca>
2022-10-31 05:45:18 -0600

    added spinner function to add/edit ingredient fragment

--- app/src/main/java/com/example/prepear/RecipeEditIngredientFragment.java
+++ app/src/main/java/com/example/prepear/RecipeEditIngredientFragment.java
@@ -6,6 +6,8 @@
  */
 package com.example.prepear;
 
+import static androidx.constraintlayout.helper.widget.MotionEffect.TAG;
+
 import android.app.AlertDialog;
 import android.app.Dialog;
 import android.content.Context;
@@ -13,7 +15,10 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -25,10 +30,12 @@ import javax.annotation.Nullable;
  */
 public class RecipeEditIngredientFragment extends DialogFragment {
     // declare variables
+    private ArrayAdapter<CharSequence> unitSpinnerAdapter;
+    private ArrayAdapter<CharSequence> categorySpinnerAdapter;
     private EditText descriptionText;
     private EditText amountText;
-    private EditText unitText;
-    private EditText categoryText;
+    private Spinner unitSpinner;
+    private Spinner categorySpinner;
     private OnFragmentInteractionListener listener;
 
     public interface OnFragmentInteractionListener {
@@ -65,18 +72,48 @@ public class RecipeEditIngredientFragment extends DialogFragment {
         TextView title = titleView.findViewById(R.id.exemptionSubHeading);
         descriptionText = view.findViewById(R.id.description_edit_text);
         amountText = view.findViewById(R.id.ingredient_amount_edit_text);
-        unitText = view.findViewById(R.id.ingredient_unit_edit_text);
-        categoryText = view.findViewById(R.id.ingredient_category_edit_text);
+        unitSpinner = view.findViewById(R.id.ingredient_unit_edit_text);
+        categorySpinner = view.findViewById(R.id.ingredient_category_edit_text);
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext()).setCustomTitle(titleView);
         title.setText("Edit Ingredient");
         Bundle bundle = getArguments();
 
+        unitSpinnerAdapter = ArrayAdapter.createFromResource(getContext(), R.array.units,
+                android.R.layout.simple_spinner_item);
+        unitSpinner.setAdapter(unitSpinnerAdapter);
+        unitSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        categorySpinnerAdapter = ArrayAdapter.createFromResource(getContext(), R.array.ingredient_categories,
+                android.R.layout.simple_spinner_item);
+        categorySpinner.setAdapter(categorySpinnerAdapter);
+        categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
         IngredientInRecipe ingredient = (IngredientInRecipe) bundle.getSerializable("ingredient");
         descriptionText.setText(ingredient.getBriefDescription());
-        amountText.setText(ingredient.getAmount());
-        unitText.setText(ingredient.getUnit());
-        categoryText.setText(ingredient.getIngredientCategory());
+        amountText.setText(ingredient.getAmount().toString());
+        unitSpinner.setSelection(unitSpinnerAdapter.getPosition(ingredient.getUnit()));
+        categorySpinner.setSelection(categorySpinnerAdapter.getPosition(ingredient.getIngredientCategory()));
 
         return builder
                 .setView(view)
@@ -86,8 +123,8 @@ public class RecipeEditIngredientFragment extends DialogFragment {
                     public void onClick(DialogInterface dialog, int which) {
                         String description = descriptionText.getText().toString();
                         int amount = Integer.parseInt(amountText.getText().toString());
-                        String unit = unitText.getText().toString();
-                        String category = categoryText.getText().toString();
+                        String unit = unitSpinner.getSelectedItem().toString();
+                        String category = categorySpinner.getSelectedItem().toString();
                         listener.onDeletePressed(new IngredientInRecipe(description, amount, unit, category));
                     }
                 })
@@ -96,8 +133,8 @@ public class RecipeEditIngredientFragment extends DialogFragment {
                     public void onClick(DialogInterface dialog, int which) {
                         String description = descriptionText.getText().toString();
                         int amount = Integer.parseInt(amountText.getText().toString());
-                        String unit = unitText.getText().toString();
-                        String category = categoryText.getText().toString();
+                        String unit = unitSpinner.getSelectedItem().toString();
+                        String category = categorySpinner.getSelectedItem().toString();
                         ingredient.setBriefDescription(description);
                         ingredient.setAmount(amount);
                         ingredient.setUnit(unit);
