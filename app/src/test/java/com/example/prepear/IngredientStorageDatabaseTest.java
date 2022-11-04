3863fd41d31c82e739d7da4d8c147826d5477dfd
Shihao Liu <lsh20011018@gmail.com>
2022-11-04 13:18:11 -0600

    Backend Ingredient Storage db testing code completion only

--- app/src/test/java/com/example/prepear/IngredientStorageDatabaseTest.java
+++ app/src/test/java/com/example/prepear/IngredientStorageDatabaseTest.java
@@ -11,6 +11,7 @@ package com.example.prepear;
 import static org.junit.jupiter.api.Assertions.assertAll;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTimeout;
@@ -61,18 +62,21 @@ public class IngredientStorageDatabaseTest {
         mockIngredientStorageCollection = mockDB.collection("Ingredient Storage");
     }
 
+    public void setMockIngredient() {
+        String mockIngredientDocumentId = DATEFORMAT.format(new Date()); // get the current date&time as the mock ingredient's document id
+        mockIngredient = new IngredientInStorage("Orange", "Fruits", "2022-11-10",
+                "Fridge","1.234556789","quantities",mockIngredientDocumentId);
+    }
+
     @Test
     @DisplayName("On below method is for testing adding a new ingredient into the storage")
     /* Create a Map object for the mock ingredient's data stored together as key-value pairs ,
      * and use this Map object to test for adding a new ingredient in Ingredient Storage Collection
      * */
     void testAddIngredientInStorage(){
-        String mockIngredientDocumentId = DATEFORMAT.format(new Date()); // get the current date&time as the mock ingredient's document id
-        mockIngredient = new IngredientInStorage("Orange", "Fruits", "2022-11-10",
-                "Fridge","1.234556789","quantities",mockIngredientDocumentId);
         Map<String, Object> mockIngredientData = new HashMap<>(); // initialize the Map object for mock ingredient's data-storage
         // On below: store every key-value pairs into the initialized Map object
-        mockIngredientData.put("document id", mockIngredientDocumentId);
+        mockIngredientData.put("document id", mockIngredient.getDocumentId());
         mockIngredientData.put("description", "Orange");
         mockIngredientData.put("bestBeforeDate", "2022-11-10");
         mockIngredientData.put("location", "Fridge");
@@ -82,7 +86,7 @@ public class IngredientStorageDatabaseTest {
 
         // On below: attempt to add the mock ingredient's Map object with its key-value paired data
         mockIngredientStorageCollection
-                .document(mockIngredientDocumentId)
+                .document(mockIngredient.getDocumentId())
                 .set(mockIngredientData)
                 .addOnSuccessListener(new OnSuccessListener<Void>() {
                     @Override
@@ -124,8 +128,69 @@ public class IngredientStorageDatabaseTest {
 
     }
 
-    /**/
 
+    /**
+     * This method will try to update an existing in-storage ingredient data inside Ingredient Storage Collection
+     */
+    @Test
+    @DisplayName("The method here is for testing editing a existing in-storage ingredient's detailed information in the Collection")
 
+    void testEditInStorageIngredientInfo(){
+        mockIngredient.setBriefDescription("Kiwi");
+        mockIngredient.setBestBeforeDate("2022-12-31");
+        mockIngredient.setLocation("Freezer");
+        mockIngredientStorageCollection
+                .document(mockIngredient.getDocumentId())
+                .update("description",mockIngredient.getBriefDescription(),
+                        "bestBeforeDate", mockIngredient.getBestBeforeDate(),
+                        "location",mockIngredient.getLocation());
+        DocumentReference mockDocRef = mockIngredientStorageCollection
+                .document(mockIngredient.getDocumentId());
+        mockDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(mockIngredient.getBriefDescription(),
+                                "DocumentSnapshot data: " + document.getData());
+                        assertEquals("Kiwi", document.get("description"));
+                        assertEquals("2022-12-31s", document.get("bestBeforeDate"));
+                        assertEquals("Freezer",document.get("location"));
+                    } else {
+                        Log.d(mockIngredient.getBriefDescription(), "No such document");
+                    }
+                } else {
+                    Log.d(mockIngredient.getBriefDescription(), "get failed with ", task.getException());
+                }
+            }
+        });
+    }
 
+    /**
+     * This method will attempt to find the deleted in-storage ingredient document from Ingredient Storage Collection
+     */
+    @Test
+    @DisplayName("The method here is for deleting an existing in-storage ingredient out of its Collection")
+
+    void testDeleteInStorageIngredient(){
+        mockIngredientStorageCollection
+                .document(mockIngredient.getDocumentId())
+                .delete();
+        DocumentReference mockDocRef = mockIngredientStorageCollection
+                .document(mockIngredient.getDocumentId());
+        mockDocRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (! document.exists()) {
+                        assert(true);
+                    }
+                } else {
+                    Log.d(mockIngredient.getBriefDescription(), "get failed with ", task.getException());
+                }
+            }
+        });
+    }
 }
