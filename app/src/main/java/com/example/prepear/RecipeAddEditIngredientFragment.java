3eb4e6c788c06311339e1e248ff525d8f316e7ac
Jamie <jamie.lee.jungeun@gmail.com>
2022-11-11 02:05:08 -0700

    updated to newer version

--- app/src/main/java/com/example/prepear/RecipeAddEditIngredientFragment.java
+++ app/src/main/java/com/example/prepear/RecipeAddEditIngredientFragment.java
@@ -15,17 +15,25 @@ import android.app.Dialog;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.DialogFragment;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import javax.annotation.Nullable;
 
@@ -41,8 +49,14 @@ public class RecipeAddEditIngredientFragment extends DialogFragment {
     private EditText descriptionText;
     private EditText amountText;
     private Spinner unitSpinner;
+    private EditText unitEditText;
+    private LinearLayout newUnitLinearLayout;
     private Spinner categorySpinner;
+    private EditText categoryEditText;
+    private LinearLayout newCategoryLinearLayout;
     private OnFragmentInteractionListener listener;
+    private TextView descriptionWordCount;
+    private TextView amountWordCount;
 
     /**
      * This method defines an interface of methods that the AddEditRecipeActivity needs to implement
@@ -103,7 +117,48 @@ public class RecipeAddEditIngredientFragment extends DialogFragment {
         descriptionText = view.findViewById(R.id.description_edit_text);
         amountText = view.findViewById(R.id.ingredient_amount_edit_text);
         unitSpinner = view.findViewById(R.id.ingredient_unit_edit_text);
+        unitEditText = view.findViewById(R.id.new_ingredient_unit_edit_text);
+        newUnitLinearLayout = view.findViewById(R.id.new_unit_linear_layout);
         categorySpinner = view.findViewById(R.id.ingredient_category_edit_text);
+        categoryEditText = view.findViewById(R.id.new_ingredient_category_edit_text);
+        newCategoryLinearLayout = view.findViewById(R.id.new_ingredient_category_linear_layout);
+        descriptionWordCount = view.findViewById(R.id.description_word_count);
+        amountWordCount = view.findViewById(R.id.amount_word_count);
+
+        /* set up word count for amount and description */
+        final TextWatcher descriptionTextEditorWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                descriptionWordCount.setText(String.valueOf(30 - charSequence.length()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        };
+        descriptionText.addTextChangedListener(descriptionTextEditorWatcher);
+        final TextWatcher amountTextEditorWatcher = new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                amountWordCount.setText(String.valueOf(10 - charSequence.length()));
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        };
+        amountText.addTextChangedListener(amountTextEditorWatcher);
 
         /* set up the unit spinner */
         unitSpinnerAdapter = ArrayAdapter.createFromResource(getContext(), R.array.units,
@@ -112,7 +167,12 @@ public class RecipeAddEditIngredientFragment extends DialogFragment {
         unitSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+                String selectedRecipeCategory = unitSpinner.getSelectedItem().toString();
+                if (selectedRecipeCategory.equals("Other")) {
+                    newUnitLinearLayout.setVisibility(View.VISIBLE);
+                } else {
+                    newUnitLinearLayout.setVisibility(View.GONE);
+                }
             }
 
             @Override
@@ -128,7 +188,12 @@ public class RecipeAddEditIngredientFragment extends DialogFragment {
         categorySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-
+                String selectedRecipeCategory = categorySpinner.getSelectedItem().toString();
+                if (selectedRecipeCategory.equals("Other")) {
+                    newCategoryLinearLayout.setVisibility(View.VISIBLE);
+                } else {
+                    newCategoryLinearLayout.setVisibility(View.GONE);
+                }
             }
 
             @Override
@@ -144,14 +209,29 @@ public class RecipeAddEditIngredientFragment extends DialogFragment {
             IngredientInRecipe ingredient = (IngredientInRecipe) bundle.getSerializable("ingredient");
             descriptionText.setText(ingredient.getBriefDescription());
             amountText.setText(String.valueOf(ingredient.getAmountString()));
-            unitSpinner.setSelection(unitSpinnerAdapter.getPosition(ingredient.getUnit()));
-            categorySpinner.setSelection(categorySpinnerAdapter.getPosition(ingredient.getIngredientCategory()));
+            String unit = ingredient.getUnit();
+            List<String> units = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.units)));
+            if (units.contains(unit)) {
+                unitSpinner.setSelection(unitSpinnerAdapter.getPosition(unit));
+            } else {
+                unitSpinner.setSelection(unitSpinnerAdapter.getPosition("Other"));
+                newUnitLinearLayout.setVisibility(View.VISIBLE);
+                unitEditText.setText(unit);
+            }
+            String ingredientCategory = ingredient.getIngredientCategory();
+            List<String> ingredientCategories = new ArrayList<>(Arrays.asList(getResources().getStringArray(R.array.ingredient_categories)));
+            if (ingredientCategories.contains(ingredientCategory)) {
+                categorySpinner.setSelection(categorySpinnerAdapter.getPosition(ingredientCategory));
+            } else {
+                categorySpinner.setSelection(categorySpinnerAdapter.getPosition("Other"));
+                newCategoryLinearLayout.setVisibility(View.VISIBLE);
+                categoryEditText.setText(ingredientCategory);
+            }
 
             /* return the edited ingredient back to AddEditRecipeActivity */
             AlertDialog.Builder builder = new AlertDialog.Builder(getContext()).setCustomTitle(titleView);
             title.setText("Edit Ingredient");
-            return builder
-                    .setView(view)
+            builder.setView(view)
                     .setNegativeButton("Cancel", null)
                     .setNeutralButton("Delete", new DialogInterface.OnClickListener() {
                         /* set listener for the Delete button, delete the clicked Ingredient
@@ -162,76 +242,95 @@ public class RecipeAddEditIngredientFragment extends DialogFragment {
                             String description = descriptionText.getText().toString();
                             String amount = amountText.getText().toString();
                             String unit = unitSpinner.getSelectedItem().toString();
+                            if (unit.equals("Other")) {
+                                unit = unitEditText.getText().toString();
+                            }
                             String category = categorySpinner.getSelectedItem().toString();
+                            if (category.equals("Other")) {
+                                category = categoryEditText.getText().toString();
+                            }
                             listener.onDeletePressed(new IngredientInRecipe(description, amount, unit, category));
                         }
                     })
                     .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                        /* set listener for the OK button, get user input,
-                        and call onOkPressed method to edit ingredient item
-                         */
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            String description;
-                            String amount;
-                            String unit;
-                            String category;
-                            if (descriptionText.getText().toString().equals("")
-                                    || amountText.getText().toString().equals("")
-                                    || unitSpinner.getSelectedItem().toString().equals("")
-                                    || categorySpinner.getSelectedItem().toString().equals("")) {
-                                Toast.makeText(getActivity().getApplicationContext(), "You did not enter full information.",
-                                        Toast.LENGTH_LONG).show();
-                            } else {
-                                description = descriptionText.getText().toString();
-                                amount = amountText.getText().toString();
-                                unit = unitSpinner.getSelectedItem().toString();
-                                category = categorySpinner.getSelectedItem().toString();
-                                ingredient.setBriefDescription(description);
-                                ingredient.setAmountValue(Double.parseDouble(amount));
-                                ingredient.setUnit(unit);
-                                ingredient.setIngredientCategory(category);
-                                listener.onOkPressed(new IngredientInRecipe(description, amount, unit, category));
-                            }
+
                         }
-                    }).create();
+                    });
+            return builder.create();
         } else {
             /* Adding new ingredient */
             AlertDialog.Builder builder = new AlertDialog.Builder(getContext()).setCustomTitle(titleView);
             title.setText("Add Ingredient");
-            return builder
-                    .setView(view)
+            builder.setView(view)
                     .setNegativeButton("Cancel", null)
                     .setPositiveButton("Confirm", new DialogInterface.OnClickListener() {
-                        /* set listener for the Confirm button, get user input,
-                        and call oncConfirmPressed method to add ingredient item
-                         */
                         @Override
                         public void onClick(DialogInterface dialog, int which) {
-                            /* get user input
-                             * and make sure that no text field is left empty
-                             * */
-                            String description;
-                            String amount;
-                            String unit;
-                            String category;
 
-                            if (descriptionText.getText().toString().equals("")
-                                    || amountText.getText().toString().equals("")
-                                    || unitSpinner.getSelectedItem().toString().equals("")
-                                    || categorySpinner.getSelectedItem().toString().equals("")) {
-                                Toast.makeText(getActivity().getApplicationContext(), "You did not enter full information.",
-                                        Toast.LENGTH_LONG).show();
-                            } else {
-                                description = descriptionText.getText().toString();
-                                amount = amountText.getText().toString();
-                                unit = unitSpinner.getSelectedItem().toString();
-                                category = categorySpinner.getSelectedItem().toString();
-                                listener.onConfirmPressed(new IngredientInRecipe(description, amount, unit, category));
-                            }
                         }
-                    }).create();
+                    });
+            return builder.create();
         }
+    }
 
+    @Override
+    public void onResume() {
+        super.onResume();
+        final AlertDialog d = (AlertDialog) getDialog();
+        if (d != null) {
+            Button positiveButton = (Button) d.getButton(Dialog.BUTTON_POSITIVE);
+            positiveButton.setOnClickListener((View v) -> {
+                Boolean wantToCloseDialog = true;
+                String description;
+                String amount;
+                String unit;
+                String category;
+                if (descriptionText.getText().toString().equals("")
+                        || amountText.getText().toString().equals("")
+                        || unitSpinner.getSelectedItem().toString().equals("")
+                        || categorySpinner.getSelectedItem().toString().equals("")) {
+                    Toast.makeText(getActivity().getApplicationContext(), "You did not enter full information.",
+                            Toast.LENGTH_LONG).show();
+                    wantToCloseDialog = false;
+                } else {
+                    Bundle bundle = getArguments();
+                    if (bundle != null) {
+                        description = descriptionText.getText().toString();
+                        amount = amountText.getText().toString();
+                        unit = unitSpinner.getSelectedItem().toString();
+                        if (unit.equals("Other")) {
+                            unit = unitEditText.getText().toString();
+                        }
+                        category = categorySpinner.getSelectedItem().toString();
+                        if (category.equals("Other")) {
+                            category = categoryEditText.getText().toString();
+                        }
+                        IngredientInRecipe ingredient = (IngredientInRecipe) bundle.getSerializable("ingredient");
+                        ingredient.setBriefDescription(description);
+                        ingredient.setAmountValue(Double.parseDouble(amount));
+                        ingredient.setUnit(unit);
+                        ingredient.setIngredientCategory(category);
+                        listener.onOkPressed(new IngredientInRecipe(description, amount, unit, category));
+                    } else {
+                        description = descriptionText.getText().toString();
+                        amount = amountText.getText().toString();
+                        unit = unitSpinner.getSelectedItem().toString();
+                        if (unit.equals("Other")) {
+                            unit = unitEditText.getText().toString();
+                        }
+                        category = categorySpinner.getSelectedItem().toString();
+                        if (category.equals("Other")) {
+                            category = categoryEditText.getText().toString();
+                        }
+                        listener.onConfirmPressed(new IngredientInRecipe(description, amount, unit, category));
+                    }
+                }
+                if (wantToCloseDialog) {
+                    d.dismiss();
+                }
+            });
+        }
     }
 }
