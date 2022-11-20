8ce0f5ca04da6c88ee6a6db05c05667a06b5a268
Jamie <jamie.lee.jungeun@gmail.com>
2022-11-19 18:21:47 -0700

    updated shopping list fragment with datepicker dialog

--- app/src/main/java/com/example/prepear/ShoppingListClickboxFragment.java
+++ app/src/main/java/com/example/prepear/ShoppingListClickboxFragment.java
@@ -16,6 +16,7 @@ import android.content.DialogInterface;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
@@ -27,8 +28,13 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDialogFragment;
 import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.FragmentManager;
 
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
 
 import javax.annotation.Nullable;
@@ -51,19 +57,23 @@ public class ShoppingListClickboxFragment extends DialogFragment {
     private Spinner locationSpinner;
     private EditText locationEditText;
     private DatePickerDialog dialog;      // create datePicker for best before date
+    private final DateFormat DATEFORMAT = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
     private ShoppingListClickboxFragment.OnFragmentInteractionListener listener;
 
     /**
      * This method defines an interface of methods that the ShoppingListViewModel needs to implement
      * in order to respond to the user clicking Ok buttons.
+     *
      * @see
      */
     public interface OnFragmentInteractionListener {
         //void onOkPressed(IngredientInStorage ingredientInStorage);
     }
+
     /**
      * This method creates a new instance of ShoppingListClickboxFragment so user can add
      * the details of ingredient by clicking checkbox
+     *
      * @param ingredient {@link IngredientInStorage} that the user clicked on
      * @return fragment the newly created fragment
      */
@@ -94,7 +104,8 @@ public class ShoppingListClickboxFragment extends DialogFragment {
     /**
      * This method creates the add ingredient fragment if the user input is valid
      * and sets errors if the input is invalid
-     * @param  savedInstanceState {@link Bundle} that stores an ingredient {@link IngredientInStorage} object
+     *
+     * @param savedInstanceState {@link Bundle} that stores an ingredient {@link IngredientInStorage} object
      * @return builder a {@link Dialog} object to build the fragment
      */
     @SuppressLint("MissingInflatedId")
@@ -149,95 +160,87 @@ public class ShoppingListClickboxFragment extends DialogFragment {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(getContext()).setCustomTitle(titleView);
         title.setText("Add Details For Ingredient");
-        builder.setView(view)
-                .setNegativeButton("Cancel", null)
-                .setPositiveButton("OK", new DialogInterface.OnClickListener() {
-                    @Override
-                    public void onClick(DialogInterface dialog, int which) {
+        builder.setView(view);
+        
+        // set date picker dialog for bestBeforeDate
+        String actualAmount = actualAmountEditText.getText().toString();
+        String bestBeforeDate = bestBeforeDateEditText.getText().toString();
+        String location = locationSpinner.getSelectedItem().toString();
 
-                    }
-                });
+        bestBeforeDateEditText.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // create a date picker for the best before date of the ingredient
+                Calendar currentDate = Calendar.getInstance();
+                int currentYear = currentDate.get(Calendar.YEAR);
+                int currentMonth = currentDate.get(Calendar.MONTH);
+                int currentDay = currentDate.get(Calendar.DAY_OF_MONTH);
+                dialog = new DatePickerDialog(getContext(), R.style.activity_date_picker,
+                        new DatePickerDialog.OnDateSetListener() {
+                            @Override
+                            public void onDateSet(DatePicker view, int year,
+                                                  int monthOfYear, int dayOfMonth) {
+                                // On below parts:
+                                // set the day of month , the month of year and the year value in the edit text
+                                // if-conditional statement helps to regulate the format of yyyy-mm-dd.
+                                if (monthOfYear < 9 && dayOfMonth < 10) {
+                                    bestBeforeDateString = year + "-" + "0" + (monthOfYear + 1) +
+                                            "-" + "0" + dayOfMonth;
+                                } else if (dayOfMonth < 10) {
+                                    bestBeforeDateString = year + "-" + (monthOfYear + 1) +
+                                            "-" + "0" + dayOfMonth;
+                                } else if (monthOfYear < 9) {
+                                    bestBeforeDateString = year + "-" + "0" + (monthOfYear + 1) +
+                                            "-" + dayOfMonth;
+                                } else {
+                                    bestBeforeDateString = year + "-" + (monthOfYear + 1) +
+                                            "-" + dayOfMonth;
+                                }
+                                bestBeforeDateEditText.setText(bestBeforeDateString);
+                            }
+                        }, currentYear, currentMonth, currentDay);
+                dialog.show();
+                /* Temporarily remove keyboards before displaying the dialog */
+                removeKeyboard();
+            }
+        });
+
+        if (location.equals("Other")) {
+            newLocationLinearLayout.setVisibility(View.VISIBLE);
+            location = locationEditText.getText().toString();
+        }
+        if (actualAmount.equals("") || bestBeforeDate.equals("") || location.equals("")) {
+            Toast.makeText(getActivity().getApplicationContext(), "You did not enter full information.",
+                    Toast.LENGTH_LONG).show();
+        } else {
+//                    IngredientInStorage ingredient = (IngredientInStorage) bundle.getSerializable("ingredient");
+//                    ingredient.setAmountValue(Double.parseDouble(actualAmount));
+//                    ingredient.setBestBeforeDate(bestBeforeDate);
+//                    ingredient.setLocation(location);
+            //listener.onOkPressed(new IngredientInStorage(ingredient.getBriefDescription(), ingredient.getIngredientCategory(), bestBeforeDate, location, actualAmount, ingredient.getUnit(), ingredient.getDocumentId(),0));
+        }
+        builder.setNegativeButton("Cancel", null);
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+            }
+        });
         return builder.create();
     }
 
     /**
-     *
-     *
+     * This method removes all present soft keyboards and is used when user clicks on one of the spinners
      */
-    @Override
-    public void onResume() {
-        super.onResume();
-        final AlertDialog d = (AlertDialog) getDialog();
-        if (d != null) {
-            Bundle bundle = getArguments();
-            Button positiveButton = (Button) d.getButton(Dialog.BUTTON_POSITIVE);
-            positiveButton.setOnClickListener((View v) -> {
-                // still needs to change bestBeforeDate to date picker
-                Boolean wantToCloseDialog = true;
-                String actualAmount = actualAmountEditText.getText().toString();
-                String bestBeforeDate = bestBeforeDateEditText.getText().toString();
-                String location = locationSpinner.getSelectedItem().toString();
-
-                bestBeforeDateEditText.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        // create a date picker for the best before date of the ingredient
-                        Calendar currentDate = Calendar.getInstance();
-                        int currentYear = currentDate.get(Calendar.YEAR);
-                        int currentMonth = currentDate.get(Calendar.MONTH);
-                        int currentDay = currentDate.get(Calendar.DAY_OF_MONTH);
-                        dialog = new DatePickerDialog(getContext(), R.style.activity_date_picker,
-                                new DatePickerDialog.OnDateSetListener() {
-                                    @Override
-                                    public void onDateSet(DatePicker view, int year,
-                                                          int monthOfYear, int dayOfMonth) {
-                                        // On below parts:
-                                        // set the day of month , the month of year and the year value in the edit text
-                                        // if-conditional statement helps to regulate the format of yyyy-mm-dd.
-                                        if (monthOfYear < 9 && dayOfMonth < 10) {
-                                            bestBeforeDateString = year + "-" + "0" + (monthOfYear + 1) +
-                                                    "-" + "0" + dayOfMonth;
-                                        } else if (dayOfMonth < 10) {
-                                            bestBeforeDateString = year + "-" + (monthOfYear + 1) +
-                                                    "-" + "0" + dayOfMonth;
-                                        } else if (monthOfYear < 9) {
-                                            bestBeforeDateString = year + "-" + "0" + (monthOfYear + 1) +
-                                                    "-" + dayOfMonth;
-                                        } else {
-                                            bestBeforeDateString = year + "-" + (monthOfYear + 1) +
-                                                    "-" + dayOfMonth;
-                                        }
-                                        bestBeforeDateEditText.setText(bestBeforeDateString);
-                                    }
-                                }, currentYear, currentMonth, currentDay);
-                        dialog.show();
-                        // Temporarily remove keyboards before displaying the dialog
-                        // removeKeyboard();
-                    }
-                });
-
-                if (location.equals("Other")) {
-                    newLocationLinearLayout.setVisibility(View.VISIBLE);
-                    location = locationEditText.getText().toString();
-                }
-                if (actualAmount.equals("") || bestBeforeDate.equals("") || location.equals("")) {
-                    Toast.makeText(getActivity().getApplicationContext(), "You did not enter full information.",
-                            Toast.LENGTH_LONG).show();
-                    wantToCloseDialog = false;
-                } else {
-                    IngredientInStorage ingredient = (IngredientInStorage) bundle.getSerializable("ingredient");
-                    ingredient.setAmountValue(Double.parseDouble(actualAmount));
-                    ingredient.setBestBeforeDate(bestBeforeDate);
-                    ingredient.setLocation(location);
-                    //listener.onOkPressed(new IngredientInStorage(ingredient.getBriefDescription(), ingredient.getIngredientCategory(), bestBeforeDate, location, actualAmount, ingredient.getUnit(), ingredient.getDocumentId(),0));
-                }
-                if (wantToCloseDialog) {
-                    d.dismiss();
-                }
-
-            });
-        }
+    private void removeKeyboard() {
+        /* get the input method manager which manages interaction within the system.
+           get System service is used to access Android system-level services like the keyboard */
+        InputMethodManager inputMethodManager =
+                (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        // Hide the soft keyboards associated with description and amount edit text fields
+        inputMethodManager.hideSoftInputFromWindow(actualAmountEditText.getWindowToken(), 0);
     }
 }
+
 
 
